24fd61eadd148fc13ec7c4e02905f4153f70f754
Ehtisham Ahmed Gondal <ehtishamahmedgondal@gmail.com>
2024-01-31 11:35:55 +0500

    seo

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the build output directory
+    sourcemap: true, // Enable source maps for debugging
+    // other build options...
+  },
+  server: {
+    port: 3000, // Specify the development server port
+    // other server options...
+  },
+  // other Vite configuration options...
+});
